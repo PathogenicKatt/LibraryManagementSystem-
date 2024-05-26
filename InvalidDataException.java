177ea4156accfbb786a2bb841ff242b030c63259
PathogenicKatt <katlehom814@gmail.com>
2024-05-26 22:04:36 +0200

    updated Member class

--- InvalidDataException.java
+++ InvalidDataException.java
@@ -1,5 +1,5 @@
-class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String m)
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String m)//the message is passed from the member class
     {
         super(m);
     }
--- Member.java
+++ Member.java
@@ -1,22 +1,30 @@
 public class Member {
-    private static int memberCounter=1; //use of static?
-    private final int memberId;
+    //private -> only accessible within that class
+    /*this -> it is a keyword used to specify the current object, the instance of a class basically in comes handy when 
+    you have variables of the same name*/
+    private static int memberCounter=1; //used to indicate that particular member belongs to that class itself rather than 
+    //as the instance of the class. Also they can be called class variables
+    private final int memberId; //makes it final because we don't want it to be changed meaning it's for that member
     private String name;
 
-    public Member()
+    public Member() //somehow a default constructor is always required.
     {
-        this("");
+        this("");/*if nothing is being passed, this constructor will by default put in an empty string which 
+        throws an exception*/
     }
     public Member(String name)
     {
         setName(name);
-        this.memberId=memberCounter++; //how is memberCounter accessible
+        this.memberId=memberCounter++; //the this key word describes the instance of the class, 
     }
+
     public void setName(String name)
     {
-        if(name==null || name.trim().isEmpty())
+        //trim() removes any leading or trailing whitespaces: " john " -> "john"
+        //isEmpty() checks if the string has a length of zero: "" -> which returns true
+        if(name==null || name.trim().isEmpty()) 
         {
-            throw new InvalidDataException("Name cannot be null or empty")
+            throw new InvalidDataException("Name cannot be null or empty");
         }
         this.name=name;
     }
@@ -29,5 +37,22 @@ public class Member {
         return "Member{"+
                 "memberId="+ memberId+
                 ", name="+name+ '\''+'}';
+    }
+
+    public static void main(String[]args)
+    {
+        Member member1,member2;
+        //whenever you create an exception u need to use the try catch block 
+        try{
+
+            member1 = new Member("katt");
+            member2 = new Member("Lulu");
+            System.out.println(member1);
+            System.out.println(member2);
+
+        }catch(InvalidDataException e)
+        {
+            System.out.println(e);
+        }
     }
 }
